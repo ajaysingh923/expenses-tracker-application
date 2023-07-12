88bf53bed3e68dfe92360f2ba21d581ef94ce696
ajay <ajay.201702@ncit.edu.np>
2023-07-13 01:25:45 +0545

    addexpensesincome

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -11,31 +11,143 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void addnewincomeandexpenses() {
+    showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              children: <Widget>[
+                TextButton(
+                  onPressed: addnewExpenses,
+                  child: Text(
+                    'Add Expenses',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: TextButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                ),
+                
+                TextButton(
+                  onPressed: addnewExpenses,
+                  child: Text('Add income',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: TextButton.styleFrom(
+                    primary: Colors.redAccent,
+                    
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text('Cancel',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ),
+                
+              ],
+            ));
+  }
+
+  void addnewIncome() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Add new income'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Amount',
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text('Save'),
+                ),
+              ],
+            ));
+  }
+
+  void addnewExpenses() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Add new expenses'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Amount',
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text('Save'),
+                ),
+              ],
+            ));
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: GetBuilder<Account>(
-      builder: (controller) {
-        controller.getdata();
-        return Container(
-            height: Get.height,
-            width: Get.width,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              colors: [
-                Colors.blue,
-                Colors.blue,
-                Color.fromARGB(255, 71, 143, 176)
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            )),
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20, Get.height * 0.08, 10, 20),
-              child: Text('Hello, ${controller.myUsername}',
-                  style: const TextStyle(
-                      fontSize: 25, fontWeight: FontWeight.bold)),
-            ));
-      },
-    ));
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: addnewincomeandexpenses,
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.add_card),
+          
+          ),
+        body: GetBuilder<Account>(
+          builder: (controller) {
+            controller.getdata();
+            return Container(
+                height: Get.height,
+                width: Get.width,
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(20, Get.height * 0.06, 10, 20),
+                  child: Text('Hello, ${controller.myUsername}',
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold)),
+                ));
+          },
+        ));
   }
 }
